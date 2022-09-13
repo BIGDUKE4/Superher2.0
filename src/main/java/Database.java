9b858b1813ce0277092ba0af50604463f004c2b4
Zana0046@stud.kea.dk <90389855+Dannicolak@users.noreply.github.com>
2022-09-13 11:42:33 +0200

    commit

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -1,8 +1,17 @@
 public class Database {
-    Superhero hero1 = new Superhero("Superman", "Super Strong", 1938, 1.91,false);
-    Superhero hero2 = new Superhero("Batman", "Rich & Strong", 1943, 1.88, true);
-    Superhero hero3 = new Superhero("Spiderman", "Spider", 1962, 1.78, true);
-    Superhero hero4 = new Superhero("Iron Man", "Rich", 1963, 1.85,true);
-    Superhero hero5 = new Superhero("Thor", "God", 1962, 1.98, false);
+
+    public Superhero [] superheroes = new Superhero[5];
+
+    public void addSuperheroes (Superhero hero1, int index){
+        superheroes [index] = hero1;
+    }
+
+
+    public String printlist (){
+        String toReturn;
+        for ()
+    }
+
+
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+
+        Superhero hero = new Superhero("Batman", "Strong", 1989, 1.89,true);
+        Superhero hero1 = new Superhero("Batman", "Strong", 1989, 1.89,true);
+        Superhero hero2 = new Superhero("Batman", "Strong", 1989, 1.89,true);
+        Superhero hero3 = new Superhero("Superman", "Strong", 1989, 1.89,true);
+        Superhero hero4 = new Superhero("Superman", "Strong", 1989, 1.89,true);
+
+
+        Database data1 = new Database();
+        data1.addSuperheroes(hero, 0);
+        data1.addSuperheroes(hero1, 1);
+        data1.addSuperheroes(hero2, 2);
+        data1.addSuperheroes(hero3, 3);
+        data1.addSuperheroes(hero4, 4);
+
         System.out.println("Velkommen til Superhero Universet");
         Scanner sc = new Scanner(System.in);
         System.out.println("Opret en Superhelt");
