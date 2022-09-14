5f6a488dd588f6c6dff90f04297b1d8af34c9db8
Zana0046@stud.kea.dk <90389855+Dannicolak@users.noreply.github.com>
2022-09-15 00:11:26 +0200

    Made som changes, will look at it tommorow again

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -2,9 +2,15 @@ import java.util.ArrayList;
 
 public class Database {
 
+    //Instansieres (Superhelte)
+    Superhero hero1 = new Superhero("Batman", "Rich & Strong", 1989, 1.88, true);
+    Superhero hero2 = new Superhero("Iron Man","Rich", 1987,1.80,true);
+    Superhero hero3 = new Superhero("Hulk", "Giant & Strong", 1990, 2.30,false);
+    Superhero hero4 = new Superhero("Thor", "God", 1992, 1.97,false);
+    Superhero hero5 = new Superhero("Spiderman", "Spider", 1980, 1.81,false);
+
     //Array til superhelte objekter
     private ArrayList<Superhero> superheroes = new ArrayList<>();
-    private int count = 0;
 
     public void addHeroes1(String superHeroName, String superPower, int creationYear, double height, boolean humanOrNot) {
         Superhero superhero = new Superhero(superHeroName, superPower, creationYear, height, humanOrNot);
@@ -16,7 +22,10 @@ public class Database {
         superheroes.add(hero);
     }
 
-    public void searchForSuperHeroes (){
+    public void searchForSuperHeroes (String searchHero){
+        for (Superhero superhero : superheroes){
+            System.out.println(get.);
+        }
 
     }
-}
+    }
