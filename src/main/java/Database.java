6891711229153d808c55e1ae517a8d394d002eed
Zana0046@stud.kea.dk <90389855+Dannicolak@users.noreply.github.com>
2022-09-15 14:21:45 +0200

    Delopgave 3

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -1,24 +1,29 @@
 import java.util.ArrayList;
 
 public class Database {
+   // private ArrayList<Superhero> superheroes;
+   // private ArrayList<Superhero> søgeresultat;
+
+
 
     //Arraylist til superhelte objekter
     private ArrayList<Superhero> superheroes = new ArrayList<>();
-    public ArrayList <Superhero> acessSuperheroes (){return superheroes;}
 
     public void addHeroes1(String superHeroName, String superPower, int creationYear, double height, boolean humanOrNot) {
         Superhero superhero = new Superhero(superHeroName, superPower, creationYear, height, humanOrNot);
        superheroes.add(superhero);
     }
 
-   // public Superhero findSuperhero (String hero) {
-       // for (Superhero superhero : superheroes) {
-       // }
-    //}
+    public Superhero findSuperhero (String hero) {
+        for (Superhero superhero : superheroes) {
+            if (superhero.getSuperheroname().equalsIgnoreCase(hero)) ;
+            return superhero;
+        }
+        return null;
+    }
     public void printData(){
         for (Superhero hero: superheroes){
             System.out.println(hero);
         }
     }
-
     }
